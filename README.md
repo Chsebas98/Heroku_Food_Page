36050a8b60a103e84d4a65fb99aee4017bbbf678
Chsebas98 <chsebas98@gmail.com>
2022-01-22 20:49:47 -0500

    finish

---
+++ README.md
@@ -0,0 +1,29 @@
+# Segundo Parcial: Food Page
+
+### INSTALACIÓN
+
+[![N|Solid](https://cldup.com/dTxpPi9lDf.thumb.png)](https://nodesource.com/products/nsolid)
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+
+Es necesario una versión de [Node.js](https://nodejs.org/) v15+ para que corra.
+
+Instala todas las dependecias y dependecias de desarrollo y pon a correr el servidor, para instalar todas las dependencias debes usar npm.
+
+Con npm
+
+```sh
+npm install
+```
+
+Y para ejecutar el programa sólo se necesita correrlo con
+
+```sh
+npm start
+```
+
+> Note: `El comando de npm start está especificado en el package.json en el apartado de Scripts.`
+> Note: `Es necesario tener instalado` [Nodemon](https://www.npmjs.com/package/nodemon) `para la ejecución del server.`
+
+## License
+
+MIT
---
+++ models/platos.js
@@ -0,0 +1,42 @@
+const Platos = [
+	{
+		id: "1",
+		name: "Pizza Hawayana",
+		price: "7.50",
+		description: "Pizza con piña.",
+	},
+	{
+		id: "2",
+		name: "Cheesy Pops",
+		price: "10.50",
+		description: "Pizza con bordes rellenos de salchichas con queso.",
+	},
+	{
+		id: "3",
+		name: "Burger de Chorizo",
+		price: "4.50",
+		description: "Hamburguesa con chorizo.",
+	},
+	{
+		id: "4",
+		name: "Triple Burger",
+		price: "8.50",
+		description:
+			"Tres pisos de una deliciosa hamburguesa con 750gr de carne de la mejor calidad, queso derretido acompañado de una porción de deliciosas papas fritas.",
+	},
+	{
+		id: "5",
+		name: "Cheesy Patty",
+		price: "2.50",
+		description: "Empanadas rellenas de queso.",
+	},
+	{
+		id: "6",
+		name: "Hot Dog",
+		price: "3.75",
+		description:
+			"Una salchicha de la mejor calidad acompañado de salsas a su gusto y un extra de ya sea trozos de papitas o picadillo de carne.",
+	},
+];
+
+module.exports = Platos;
